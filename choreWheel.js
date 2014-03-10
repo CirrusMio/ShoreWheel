b4787f0446937cd0a9b353d1025d93ec6431e78d
Michael Huelsman <michael.huelsman@gmail.com>
2014-03-10 16:23:42 -0400

    Finished the chore wheel class

--- choreWheel.js
+++ choreWheel.js
@@ -1,4 +1,28 @@
 function ChoreWheel(){
-  this.chores = {};
-  this.people = {};
+  this.chores = [];
+  this.people = [];
+  this.pairs = [];
+};
+
+ChoreWheel.prototype.setPairs = function(){
+  this.pairs = [];
+  for(var i = 0;i < people.length;i++){
+    this.pairs.push([chores[i],people[i]]);
+  }
+};
+
+ChoreWheel.prototype.getPairs = function(){
+  return this.pairs;
+}
+
+ChoreWheel.prototype.addPerson = function(name){
+  this.people.push(name);
+};
+
+ChoreWheel.prototype.addChore = function(name){
+  this.chores.push(name);
+};
+
+ChoreWheel.prototype.rotateWheel = function(){
+  this.people.push(this.people.shift());
 };
