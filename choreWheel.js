99898720a63a2202b11c954f68b82cf4d329f456
Michael Huelsman <michael.huelsman@gmail.com>
2014-03-12 12:45:17 -0400

    Added a simple frontend still working out the kinks

--- choreWheel.js
+++ choreWheel.js
@@ -1,13 +1,15 @@
 function ChoreWheel(){
-  this.chores = [];
-  this.people = [];
+  this.chores = ['Dishes','Garbage','Recycling','Stuff'];
+  this.people = ['Mike','Will','Tanzi','Sarah'];
   this.pairs = [];
+  // This variable remembers the children that need to be removed
+  this.children = [];
 };
 
 ChoreWheel.prototype.setPairs = function(){
   this.pairs = [];
-  for(var i = 0;i < people.length;i++){
-    this.pairs.push([chores[i],people[i]]);
+  for(var i = 0;i < this.people.length;i++){
+    this.pairs.push([this.chores[i],this.people[i]]);
   }
 };
 
@@ -25,4 +27,20 @@ ChoreWheel.prototype.addChore = function(name){
 
 ChoreWheel.prototype.rotateWheel = function(){
   this.people.push(this.people.shift());
+  this.setPairs();
+  var str = '';
+  for(var i = 0;i < this.children.length;i++){
+    document.getElementById("WHEEL").removeChild(this.children[i]);
+  }
+  for(var i = 0;i < this.pairs.length;i++){
+    str = this.pairs[i][0] + " " + this.pairs[i][1];
+    var pair = document.createTextNode(str);
+    var para = document.createElement("p");
+    para.appendChild(pair);
+    this.children.push(para);
+    document.getElementById("WHEEL").appendChild(para);
+  }
 };
+
+var wheel =new ChoreWheel();
+wheel.setPairs();
