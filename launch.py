466771c5eb4bfea371819256980e2fc5a17cbae0
Michael Huelsman <michael.huelsman@gmail.com>
2014-03-27 14:07:19 -0400

    Added simple db interaction wrapper

--- launch.py
+++ launch.py
@@ -1,9 +1,13 @@
+import os
+os.pathsep.append("./db/")
+
 from flask import Flask
+from interactDB import getPeople
 app = Flask('ShoreWheel')
 
 @app.route('/')
 def index():
-  return "Hello, World"
+  return getPeople()
 
 
 app.run()
