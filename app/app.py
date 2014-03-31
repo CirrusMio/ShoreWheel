d5ad151a0b62fc137191ffe522e5ca4c15b96feb
Michael Huelsman <michael.huelsman@gmail.com>
2014-03-31 16:55:05 -0400

    Fixed last problem

--- app/app.py
+++ app/app.py
@@ -2,6 +2,6 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask('ShoreWheel')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 
 db = SQLAlchemy(app)
