0a66a8e6726246541cc8b934ac7359c1bffb4ceb
Michael Huelsman <michael.huelsman@gmail.com>
2014-03-31 16:19:34 -0400

    Finished up converting createDb.py

--- db/createDB.py
+++ db/createDB.py
@@ -1,35 +1,40 @@
 from app import db
 
 class Person(db.Model):
-  __tablename__ = "people"
-
   displayName = db.Column(db.String)
   fullName = db.Column(db.String)
   tickets = db.Column(db.Integer)
   id = db.Column(db.Integer, primary_key=True)
 
+  def __init__(self, fullname, displayName):
+    self.fullName = fullname
+    self.displayName = displayName
+    self.tickets = 1
 
   def __repr__(self):
     return "<Person(displayName='%s', fullName='%s', tickets='%i')>" % (self.displayName, self.fullName)
 
 class Chore(db.Model):
-  __tablename__ = "chores"
-
   name = db.Column(db.String)
   #frequency expressed in weeks
   freq = db.Column(db.Integer)
   id = db.Column(db.Integer, primary_key=True)
+
+  def __init__(self, name, freq):
+    self.name = name
+    self.freq = freq
 
   def __repr__(self):
     return "<Chore(name='%s', freq='%i')>" % (self.name, self.freq)
 
 #represents a person Chore pair
 class Pair(db.Model):
-  __tablename__ = "pairs"
-
   person = db.Column(db.Integer)
   chore = db.Column(db.Integer, primary_key=True)
 
+  def __init__(self, person, chore):
+    self.person = person
+    self.chore = chore
 
   def __repr__(self):
     return "<Pair(chore='%i', person='%i')>" % (self.person, self.chore)
