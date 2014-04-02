7e183f59630056b8773c8df594bd1088ce9d36b4
Michael Huelsman <michael.huelsman@gmail.com>
2014-04-02 16:54:08 -0400

    Added the multiweek chore selection algorithm

--- db/interactDB.py
+++ db/interactDB.py
@@ -1,6 +1,6 @@
 from createDB import Person, Chore
 from app import db
-
+from random import randint
 
 
 #adds the chore specified by the name, n, and the frequency, f.
@@ -23,8 +23,32 @@ def rotate():
     i += 1
   return
 
+#shuffles an array making lottery more random
+def shuffle(lst):
+  result = []
+  while(len(lst) != 0):
+    result.append( lst.pop( randint(0, len(lst)-1) ) )
+  return result
+
+
 def multiSelect():
   #TODO func selects people for the multi-week chores
+  totalTickets = 0
+  people = Person.query.all()
+  people = shuffle(people)
+  for p in people:
+    totalTickets += p.tickets
+
+  chores = Chore.query.filter(Chore.freq != 1)
+  #use a modified lottery system to determine assignments
+  for c in chores:
+    lottery = randint(1,totalTickets)
+    for p in people:
+      lottery -= p.tickets
+      if(lottery <= 0):
+        p.tickets -= c.freq
+        totalTickets -= c.freq
+        c.assigned = p.displayName
   return
 
 
