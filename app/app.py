2bc540bac5afd4cc2147affd2084fcb3d7eccb82
theWittmer <tyler.arnold@cirrusmio.com>
2014-04-08 16:43:29 -0400

    Using CDNs now thanks to Sarah's guidance

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.bootstrap import Bootstrap
 
 app = Flask('ShoreWheel')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
