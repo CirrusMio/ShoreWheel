b6075f287a9bd013bb578f3ebbae5fb364caf0ad
Michael Huelsman <michael.huelsman@gmail.com>
2014-04-08 14:52:29 -0400

    rebasing

--- db/createDB.py
+++ db/createDB.py
@@ -19,15 +19,18 @@ class Chore(db.Model):
   #frequency expressed in weeks
   freq = db.Column(db.Integer)
   assigned = db.Column(db.String)
+  lastRotate = db.Column(db.Date)
+  untilRotate = db.Column(db.Integer)
   id = db.Column(db.Integer, primary_key=True)
 
-  def __init__(self, name, freq):
+  def __init__(self, name, freq, lastRotate):
     self.name = name
     self.freq = freq
+    self.untilRotate = 0
     self.assigned = ""
+    self.lastRotate = lastRotate
 
   def __repr__(self):
-    return "<Chore(name='%s', freq='%i', assigned='%s')>" % (self.name, self.freq, self.assigned)
-
+    return "<Chore(name='%s', freq='%i', assigned='%s', untilRotate=%i)>" % (self.name, self.freq, self.assigned, self.untilRotate)
 
 db.create_all()
--- launch.py
+++ launch.py
@@ -2,11 +2,21 @@ from app import app
 from flask import Flask, render_template, redirect, url_for
 from interactDB import getPeople, getChores, rotate, multiSelect
 from seedDB import seed
+from datetime import timedelta, date
+
+def update():
+  for c in getChores():
+    if(c.lastRotate-date.today() > timedelta(weeks=1)):
+      c.untilRotate -= int((c.lastRotate-date.today()).days/7)
+      c.lastRotate = date.today()
 
 @app.route('/')
 def index():
   people = getPeople()
   chores = getChores()
+  update()
+  rotate()
+  multiSelect()
   return render_template('template.html', people = people, chores = chores)
 
 @app.route('/spin')
