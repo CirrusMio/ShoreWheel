ab72c5c4bac3b8494240926f1c871b4c19fcb5a5
Michael Huelsman <michael.huelsman@gmail.com>
2014-04-29 16:36:48 -0400

    Still trying to fix this

--- cookbook/recipes/run.rb
+++ cookbook/recipes/run.rb
@@ -20,3 +20,25 @@ bash 'seed-project' do
   make seed
   EOH
 end
+
+define :run do
+  app_name = params[:name]
+
+  template ::File.join('/etc/init.d/', "#{app_name}-gunicorn") do
+    source 'gunicorn-init.erb'
+    owner 'root'
+    group 'root'
+    mode 0755
+    variables({
+      deploy_path: params[:deploy_path],
+      environment: params[:environment]
+    })
+    action :create
+  end
+
+  service "#{app_name}-gunicorn" do
+    init_command "/etc/init.d/#{app_name}-gunicorn"
+    supports restart: true, reload: true
+    action :enable
+  end
+end
